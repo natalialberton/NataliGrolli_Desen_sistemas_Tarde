18f75d1eea5c49b9034eb79e5efbadd029285369
Natalí Alberton <alberton.natali@gmail.com>
2025-05-05 15:38:39 -0300

    arrumando erros (TESTES)

--- TESTES/pratica1/test_cadastro_cliente.py
+++ TESTES/pratica1/test_cadastro_cliente.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 
 #Acesso a página de cadastro usando o caminho absoluto com o protocolo file://
 #Certifique-se de que o caminho está apontando para um arquivo HTML específico
-driver.get("file:///C:\Users\natali_grolli\Documents\GitHub\NataliGrolli_Desen_sistemas_Tarde\TESTES\pratica1\index.html")
+driver.get("file:///C:/Users/natali_grolli/Documents/GitHub/NataliGrolli_Desen_sistemas_Tarde/TESTES/pratica1/index.html")
 
 #Preenche o campo Nome
 nome_input = driver.find_element(By.ID, "name")
@@ -30,7 +30,7 @@ submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 submit_button.click()
 
 #Aguarda um momento para visualizar o resultado (em uma aplicação real, você verificaria a resposta)
-time.sleep(3)
+time.sleep(3000)
 
 #Fecha o navegador
-driver.quit()
+#driver.quit()
