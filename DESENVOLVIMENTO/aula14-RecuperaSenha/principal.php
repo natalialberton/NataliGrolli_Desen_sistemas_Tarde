7e751b92db94116673b003a5f25354e8333c35f6
Natalí Alberton <alberton.natali@gmail.com>
2025-05-20 16:43:30 -0300

    Aula 14 - Recuperação de Senha
    Aula do dia 14/05

---
+++ DESENVOLVIMENTO/aula14-RecuperaSenha/principal.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: login.php");
+        echo "<script>alert('Você precisa estar logado para acessar esta página.');</script>";
+        exit();
+    }
+
+    // OBTENDO O NOME DO PERFIL DO USUÁRIO LOGADO
+    $id_perfil = $_SESSION['perfil'];
+    $sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+    $stmtPerfil = $pdo->prepare($sqlPerfil);
+    $stmtPerfil->bindParam(":id_perfil", $id_perfil);
+    $stmtPerfil->execute();
+    $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+    $nome_perfil = $perfil['nome_perfil'];
+
+    // DEFINIÇÃO DAS PERMISSÕES POR PERFIL
+    $permissoes = [
+        //ADMINISTRADOR
+        '1' =>
+        ['Cadastrar' =>[
+            "cadastro_usuario.php",
+            "cadastro_perfil.php",
+            "cadastro_cliente.php",
+            "cadastro_fornecedor.php",
+            "cadastro_produto.php",
+            "cadastro_funcionario.php"
+        ] ],
+        ['Buscar' =>[
+            "buscar_usuario.php",
+            "buscar_perfil.php",
+            "buscar_cliente.php",
+            "buscar_fornecedor.php",
+            "buscar_produto.php",
+            "buscar_funcionario.php"
+        ] ],
+        ['Alterar' =>[
+            "alterar_usuario.php",
+            "alterar_perfil.php",
+            "alterar_cliente.php",
+            "alterar_fornecedor.php",
+            "alterar_produto.php",
+            "alterar_funcionario.php"
+        ] ],
+        ['Excluir' =>[
+            "excluir_usuario.php",
+            "excluir_perfil.php",
+            "excluir_cliente.php",
+            "excluir_fornecedor.php",
+            "excluir_produto.php",
+            "excluir_funcionario.php"
+        ] ],
+
+        //SECRETARIA
+        '2' =>
+        ['Cadastrar' =>[
+            "cadastro_cliente.php"
+        ] ],
+        ['Buscar' =>[
+            "buscar_cliente.php",
+            "buscar_fornecedor.php",
+            "buscar_produto.php"
+        ] ],
+        ['Alterar' =>[
+            "alterar_cliente.php",
+            "alterar_fornecedor.php"
+        ] ],
+
+        //ALMOXARIFADO
+        '3' =>
+        ['Cadastrar' =>[
+            "cadastro_fornecedor.php",
+            "cadastro_produto.php"
+        ] ],
+        ['Buscar' =>[
+            "buscar_fornecedor.php",
+            "buscar_produto.php"
+        ] ],
+        ['Alterar' =>[
+            "alterar_fornecedor.php",
+            "alterar_produto.php"
+        ] ],
+        ['Excluir' =>[
+            "excluir_fornecedor.php",
+            "excluir_produto.php"
+        ] ],
+
+    ]
+?>
