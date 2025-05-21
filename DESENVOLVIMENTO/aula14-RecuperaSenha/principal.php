846b10a9b501225c37fbdf99aef6b787163cc395
Natalí Alberton <alberton.natali@gmail.com>
2025-05-21 17:06:32 -0300

    Aula 16 - Sistema CRUD
    Desenvolvimento das páginas:
    - Principal
    - Cadastramento de Usuário
    - Buscar Usuário

--- DESENVOLVIMENTO/aula14-RecuperaSenha/principal.php
+++ DESENVOLVIMENTO/aula14-RecuperaSenha/principal.php
@@ -22,30 +22,30 @@ require_once 'conexao.php';
         //ADMINISTRADOR
         '1' =>
         ['Cadastrar' =>[
-            "cadastro_usuario.php",
-            "cadastro_perfil.php",
-            "cadastro_cliente.php",
-            "cadastro_fornecedor.php",
-            "cadastro_produto.php",
-            "cadastro_funcionario.php"
-        ] ],
-        ['Buscar' =>[
+            "cadastrar_usuario.php",
+            "cadastrar_perfil.php",
+            "cadastrar_cliente.php",
+            "cadastrar_fornecedor.php",
+            "cadastrar_produto.php",
+            "cadastrar_funcionario.php"
+        ],
+        'Buscar' =>[
             "buscar_usuario.php",
             "buscar_perfil.php",
             "buscar_cliente.php",
             "buscar_fornecedor.php",
             "buscar_produto.php",
             "buscar_funcionario.php"
-        ] ],
-        ['Alterar' =>[
+        ],
+        'Alterar' =>[
             "alterar_usuario.php",
             "alterar_perfil.php",
             "alterar_cliente.php",
             "alterar_fornecedor.php",
             "alterar_produto.php",
             "alterar_funcionario.php"
-        ] ],
-        ['Excluir' =>[
+        ],
+        'Excluir' =>[
             "excluir_usuario.php",
             "excluir_perfil.php",
             "excluir_cliente.php",
@@ -58,13 +58,13 @@ require_once 'conexao.php';
         '2' =>
         ['Cadastrar' =>[
             "cadastro_cliente.php"
-        ] ],
-        ['Buscar' =>[
+        ],
+        'Buscar' =>[
             "buscar_cliente.php",
             "buscar_fornecedor.php",
             "buscar_produto.php"
-        ] ],
-        ['Alterar' =>[
+        ],
+        'Alterar' =>[
             "alterar_cliente.php",
             "alterar_fornecedor.php"
         ] ],
@@ -74,19 +74,74 @@ require_once 'conexao.php';
         ['Cadastrar' =>[
             "cadastro_fornecedor.php",
             "cadastro_produto.php"
-        ] ],
-        ['Buscar' =>[
+        ],
+        'Buscar' =>[
+            "buscar_cliente.php",
             "buscar_fornecedor.php",
             "buscar_produto.php"
-        ] ],
-        ['Alterar' =>[
+        ],
+        'Alterar' =>[
             "alterar_fornecedor.php",
             "alterar_produto.php"
-        ] ],
-        ['Excluir' =>[
+        ],
+        'Excluir' =>[
             "excluir_fornecedor.php",
             "excluir_produto.php"
         ] ],
 
-    ]
+        //CLIENTE
+        '4' =>
+        ['Cadastrar' =>[
+            "cadastro_cliente.php",
+        ],
+        'Buscar' =>[
+            "buscar_produto.php",
+        ],
+        'Alterar' =>[
+            "alterar_cliente.php",
+        ] ],
+    ];
+
+    //OBTENDO AS OPÇÕES DISPONÍVEIS PARA O PERFIL LOGADO
+    $opcoes_menu = $permissoes[$id_perfil];
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAINEL PRINCIPAL</title>
+    <link rel="stylesheet" href="./styles.css">
+    <script src="scripts.js"></script>
+</head>
+<body>
+    <header>
+        <div class="saudacao">
+            <h2>Bem-vindo, <?php echo $_SESSION["usuario"];?>! Perfil: <?php echo $nome_perfil;?></h2>
+        </div>
+
+        <div class="logout">
+            <form action="logout.php" method="POST">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
+    </header>
+    <nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria=>$arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?= $arquivo?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php")))?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+</body>
+</html>
