6b756d2dd018f0f56e983c91dbe7fd463b072895
Natalí Alberton <139291235+natalialberton@users.noreply.github.com>
2025-06-03 15:00:24 +0000

    commit

--- DESENVOLVIMENTO/aula16-ImgCrud/cadastrar_funcionario.php
+++ DESENVOLVIMENTO/aula16-ImgCrud/cadastrar_funcionario.php
@@ -7,27 +7,29 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Cadastro</h1>
-        <h2>Funcionário</h2>
-        <!--FORMULÁRIO PARA CADASTRAR FUNCIONÁRIO-->
-        <form action="salvar_funcionario.php" method="POST" enctype="multipart/form-data">
-           
-            <!--CAMPO PARA INSERIR O NOME DO FUNCIONÁRIO-->
-            <label for="nome">Nome: </label>
-            <input type="text" name="nome" id="nome" required><br>
+    <main>
+        <div class="container-cadastro">
+            <h1>Cadastro</h1>
+            <h2>Funcionário</h2>
+            <!--FORMULÁRIO PARA CADASTRAR FUNCIONÁRIO-->
+            <form class="forms-cadastro" action="salvar_funcionario.php" method="POST" enctype="multipart/form-data">
+            
+                <!--CAMPO PARA INSERIR O NOME DO FUNCIONÁRIO-->
+                <label class="forms-cadastro__label" for="nome">Nome: </label>
+                <input class="forms-cadastro__input" type="text" name="nome" id="nome" required><br>
 
-            <!--CAMPO PARA INSERIR O TELEFONE DO FUNCIONÁRIO-->
-            <label for="telefone">Telefone: </label>
-            <input type="tel" name="telefone" id="telefone" required><br>
+                <!--CAMPO PARA INSERIR O TELEFONE DO FUNCIONÁRIO-->
+                <label class="forms-cadastro__label" for="telefone">Telefone: </label>
+                <input class="forms-cadastro__input" type="tel" name="telefone" id="telefone" required><br>
 
-            <!--CAMPO PARA FAZER UPLOAD DA FOTO DO FUNCIONÁRIO-->
-            <label for="foto">Foto: </label>
-            <input type="file" name="foto" id="foto" required><br>
+                <!--CAMPO PARA FAZER UPLOAD DA FOTO DO FUNCIONÁRIO-->
+                <label class="forms-cadastro__label" for="foto">Foto: </label>
+                <input class="forms-cadastro__input" type="file" name="foto" id="foto" required><br>
 
-            <!--BOTÃO PARA ENVIAR O FORMULÁRIO-->
-             <button type="submit">Cadastrar</button>
-        </form>
-    </div>
+                <!--BOTÃO PARA ENVIAR O FORMULÁRIO-->
+                <button class="forms-cadastro__btn-cadastrar" type="submit">Cadastrar</button>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
